30cbd746e7a4b1b21a91a05ba2a4edfeb2fe6cb2
bijan3s <89301589+bijan3s@users.noreply.github.com>
2021-09-17 14:41:50 +0430

    Update urls.py

--- django_project/urls.py
+++ django_project/urls.py
@@ -23,6 +23,6 @@ import debug_toolbar
 urlpatterns = [
     path('Zzmp9BjuRmtsdrey/', admin.site.urls),
     path('', include('ecommerce_app.urls')),
-    path('__debug__/', include(debug_toolbar.urls)),
+  
 
 ] + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
